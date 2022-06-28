68068f40a36288d18b2a568061fba8ee287bdb4e
ralphb24 <107438645+ralphb24@users.noreply.github.com>
2022-06-29 11:08:22 +1200

    Add files via upload

--- Quiz.py
+++ Quiz.py
@@ -1,17 +1,25 @@
+from ast import And
 from doctest import SkipDocTestCase
 from tkinter import *
 import random
 from tkinter import scrolledtext
 from tkinter.font import Font
 from unittest import skip
+from kivy import require
 from matplotlib.markers import MarkerStyle
 from matplotlib.pyplot import close
 from numpy import append
 
-questionsList = [["Earth is the 3rd planet from the Sun.","True","False"],["How many days does it take for the Earth to revolve around the Sun?","365","366","364","356"],["How long does it take for the Earth to fully rotate around its own axis?","24 hours","25 hours","23 hours","42 hours"],["The moon takes 29.5 days to complete one orbit around the Earth.","True","False"],["Why does the Moon have a greater effect on tides\non Earth compared to the Sun?","Because the Moon is closer to the Earth","Because the Sun is too hot","Because the Moon is smaller","Because the Sun is bright"],["Who was the first human to set foot on the Moon?","Neil Armstrong","Sergey Korsakov","Robert Hines","Samantha Cristoforetti"],["How many kilometres is 1 Astronomical Unit (A.U.)?","150 000 000 km","100 000 000 km","999 999 999 km","500 000 km"],["The moon produces light of its own.","False","True"],["'Gibbous' is used to describe the Moon when more than half (but not all)\nof the Moon is illuminated.","True","False"],["'Waxing' is used to describe the area of the Moon you can see\nbecoming larger each night.","True","False"],["'Waning' is used to describe the area of the Moon you can see\nbecoming smaller each night.","True","False"],["What causes the seasons on Earth?","The Earth's tilt with respect to the Sun","The gravitational pull of the Sun","The speed of which the Earth orbits the Sun","The distance between the Earth and the Sun"],["Regular changes in sea levels (tides) occur due to the\ngravitational pull of the Moon and the Sun.","True","False"],["Which two pieces of evidence did people use to believe that the\nApollo 11 Moon landing was a hoax?","The flag and the stars","The flag and the Sun","The stars and the Sun","Their footprints and astronaut suits"],["How many phases of the Moon are there?","8","6","12","10"],["Which element does the Sun consume to release energy?","Hydrogen","Carbon","Nitrogen","Oxygen"],["Sunspots are areas that appear dark on the Sun.","True","False"],["Which of the following is Pluto considered as?","A dwarf planet","A planet","An asteroid","A comet"],["Is Astrology a psuedoscience?","True","False"],["How many planets are there in our solar system?","8","4","15","9"]]
-resultsMsg = ["TOUGH LUCK","NICE TRY","WELL DONE","SPECTACULAR"]
-overallMark = ["NOT ACHIEVED","ACHIEVED","MERIT","EXCELLENCE"]
-requiredScore = [0,4,7,10,12]
+def establishLists():
+    global questionsList
+    global resultsMsg
+    global markList
+    global requiredScore
+    questionsList = [["Earth is the 3rd planet from the Sun.","True","False"],["How many days does it take for the Earth to revolve around the Sun?","365","366","364","356"],["How long does it take for the Earth to fully rotate around its own axis?","24 hours","25 hours","23 hours","42 hours"],["The moon takes 29.5 days to complete one orbit around the Earth.","True","False"],["Why does the Moon have a greater effect on tides\non Earth compared to the Sun?","Because the Moon is closer to the Earth","Because the Sun is too hot","Because the Moon is smaller","Because the Sun is bright"],["Who was the first human to set foot on the Moon?","Neil Armstrong","Sergey Korsakov","Robert Hines","Samantha Cristoforetti"],["How many kilometres is 1 Astronomical Unit (A.U.)?","150 000 000 km","100 000 000 km","999 999 999 km","500 000 km"],["The moon produces light of its own.","False","True"],["'Gibbous' is used to describe the Moon when more than half (but not all)\nof the Moon is illuminated.","True","False"],["'Waxing' is used to describe the area of the Moon you can see\nbecoming larger each night.","True","False"],["'Waning' is used to describe the area of the Moon you can see\nbecoming smaller each night.","True","False"],["What causes the seasons on Earth?","The Earth's tilt with respect to the Sun","The gravitational pull of the Sun","The speed of which the Earth orbits the Sun","The distance between the Earth and the Sun"],["Regular changes in sea levels (tides) occur due to the\ngravitational pull of the Moon and the Sun.","True","False"],["Which two pieces of evidence did people use to believe that the\nApollo 11 Moon landing was a hoax?","The flag and the stars","The flag and the Sun","The stars and the Sun","Their footprints and astronaut suits"],["How many phases of the Moon are there?","8","6","12","10"],["Which element does the Sun consume to release energy?","Hydrogen","Carbon","Nitrogen","Oxygen"],["Sunspots are areas that appear dark on the Sun.","True","False"],["Which of the following is Pluto considered as?","A dwarf planet","A planet","An asteroid","A comet"],["Astrology is a psuedoscience.","True","False"],["How many planets are there in our solar system?","8","4","15","9"]]
+    resultsMsg = ["TOUGH LUCK!","NICE TRY!","WELL DONE!","SPECTACULAR!","Invalid"]
+    markList = ["NOT ACHIEVED","ACHIEVED","MERIT","EXCELLENCE"]
+    requiredScore = [0,4,7,10,12]
+    setUp()
 
 def detQstns():
     global root
@@ -153,21 +161,50 @@ def calcScore(answer):
 def nextQstn():
     qRoot.destroy()
 
+def retry():
+    resultsRoot.destroy()
+    establishLists()
+
+
 def calcMark():
+    global endMsg
+    global overallMark
     global resultsRoot
     resultsRoot = Tk()
     resultsRoot.title("BALAZO SCIENCE QUIZ")
     resultsRoot.configure(bg="#4a86e8")
     resultsRoot.geometry('1000x700')
     pageFont = Font(family="Calibri", size=20, weight="bold")
+
+    if score >= requiredScore[0] and score < requiredScore[1]:  
+        overallMark = markList[0]
+        endMsg = resultsMsg[0]
+    elif score >= requiredScore[1] and score < requiredScore[2]:
+        overallMark = markList[1]
+        endMsg = resultsMsg[1]
+    elif score >= requiredScore[2] and score < requiredScore[3]:
+        overallMark = markList[2]
+        endMsg = resultsMsg[2]
+    elif score >= requiredScore[3] and score <= requiredScore[4]:
+        overallMark = markList[3]
+        endMsg = resultsMsg[3]
+    else:
+        endMsg = resultsMsg[4]
+        overallMark = "Invalid"
     
     endTitle = Label(resultsRoot, text="QUIZ FINISHED!", font=pageFont, fg="#4a86e8", bg="#cfe2f3", padx=80, pady=25)
     endTitle.place(x=300, y=60)
 
-    endResults = Label(resultsRoot, text="QUESTIONS CORRECT:", font=pageFont, fg="#4a86e8", bg="#cfe2f3", padx=80, pady=25)
+    endResults = Label(resultsRoot, text="QUESTIONS CORRECT:\n"+str(score)+"/12\n\nOVERALL MARK:\n"+str(overallMark)+"\n\n"+str(endMsg), font=pageFont, fg="#4a86e8", bg="#cfe2f3", padx=80, pady=25)
     endResults.place(x=300, y=400)
 
-setUp()
+    retryBut = Button(resultsRoot, text="RETRY", font=pageFont, fg="#ffffff", bg="#6aa84f", padx=80, pady=5, borderwidth=0, command=retry)
+    retryBut.place(x=100, y=600)
+
+    exitBut = Button(resultsRoot, text="EXIT", font=pageFont, fg="#ffffff", bg="#cc0000", padx=80, pady=5, borderwidth=0, command=exit)
+    exitBut.place(x=700, y=600)
+
+establishLists()
 
 
 
